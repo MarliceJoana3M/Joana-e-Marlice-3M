11a63c19bf0c1bd77d3513cf9dbe5dec81443d83
MarliceJoana3M <47109711+MarliceJoana3M@users.noreply.github.com>
2019-01-28 13:16:01 +0000

    Add files via upload

---
+++ av 8 joana palmeiro/js.php
@@ -0,0 +1,179 @@
+<?php 
+    
+    $titulo = 'JS - javascript';
+    $t_header = 'JS <span>javascript</span>';
+
+    include 'includes/header.php' 
+    
+    ?>
+
+<section id="rp">
+    <h2>Tipos de dados</h2>
+    <div class="snippets">
+        <div class="boxes">
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>Number</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>O Numero pertence a JavaScript e é um objeto wrapper que trabalha com valores numéricos. Um Number é criado usando o Number()construtor . 
+					Um número de objeto do tipo primitivo foi criado para usar a função Number () .
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>Boolean</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>O Boolean é um wrapper de objeto para um valor booleano.
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>String</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>O String  é um construtor para seqüências de caracteres.
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>undefined</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>A undefined é uma propriedade que representa o valor primitivo. É um dos tipos primitivos do JavaScript.
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>null</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>  Null é um valor primitivo. Como JavaScript é sensível a maiúsculas, null não é a mesma coisa que Null, NULL, ou qualquer outra variante
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+        </div>
+    </div>
+</section>
+
+<section id="de">
+    <h2>Operadores</h2>
+    <div class="snippets">
+        <div class="boxes">
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>Atribuição</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p> atribuição ( =) atribui um valor a uma variável.
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>Adição</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p> adição ( +) adiciona números:
+
+
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        <code>Multiplicação</code>
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p> multiplicação ( *) multiplica números.
+
+
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+                 
+          </div>
+</section>
+
+<section id="cw">
+    <h2>Objetos</h2>
+    <div class="snippets">
+        <div class="boxes">
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                    <h3>
+                        
+                    </h3>
+                </div>
+                <div class="sb-bot">
+                    <p>
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+        </div>
+    </div>
+</section>
+
+
+<section id="ct">
+    <h2>Eventos</h2>
+    <div class="snippets">
+        <div class="boxes">
+            <!-- box -->
+            <div class="s-box ">
+                <div class="sb-top">
+                </div>
+                <div class="sb-bot">
+                    <p>
+                    </p>
+                </div>
+            </div>
+            <!-- ./box -->
+        </div>
+    </div>
+</section>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
